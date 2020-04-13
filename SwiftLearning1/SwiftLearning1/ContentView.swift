7283170370f23822e123f8013ac6fc50f419761f
Alireza <appleiran66@gmail.com>
2020-04-13 22:44:31 +0430

    Start

---
+++ SwiftLearning1/SwiftLearning1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftLearning1
+//
+//  Created by ali reza on 1/25/1399 AP.
+//  Copyright © 1399 ali reza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftLearning1/SwiftLearning1Tests/SwiftLearning1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftLearning1Tests.swift
+//  SwiftLearning1Tests
+//
+//  Created by ali reza on 1/25/1399 AP.
+//  Copyright © 1399 ali reza. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftLearning1
+
+class SwiftLearning1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
